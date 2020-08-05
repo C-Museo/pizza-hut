612d72da59ecc007066d8a7df8badf788833a096
comfort museo <you@example.com>
2020-08-05 16:51:01 +0300

    modify checkout

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 function verifyOder()
 {
-var opts = document.getElementById("myselection"); 
-opts.style="display:block;";
 
 var pizza = document.getElementById("pizza").value;  
 var size = document.getElementById("size").value; 
 var crust = document.getElementById("crust").value; 
 var toppings = document.getElementById("toppings").value; 
-var deliveryYes = document.getElementById("yes").value; 
-var deliveryNo = document.getElementById("no").value; 
+var delivery = document.getElementById("delivery").value; 
+
 if(pizza!="" || size!=""| crust!="" | toppings !="") 
 {
   
 
-if(deliveryYes!="" | deliveryNo !="")
+if(delivery!="")
 {
-
+    var opts = document.getElementById("myselection"); 
+    opts.style="display:block;";
 
 //results
 var myPizza = document.getElementById("myPizza"); 
@@ -23,22 +22,66 @@ var myPizzaSize = document.getElementById("myPizzaSize");
 var myToppings = document.getElementById("myToppings"); 
 var myCrust = document.getElementById("myCrust"); 
 var myDeliveryOption = document.getElementById("myDeliveryOption"); 
+var amount = document.getElementById("amount"); 
 myPizza.innerHTML="";
 myPizzaSize.innerHTML="";
 myToppings.innerHTML="";
 myCrust.innerHTML="";
+myDeliveryOption.innerHTML="";
+amount.innerHTML="";
 
 myPizza.innerHTML=pizza;
-myPizzaSize.innerHTML=size;
+
 myToppings.innerHTML=toppings;
-myCrust.innerHTML=crust;
-if(deliveryNo !="")
+if(size=="1200")
 {
-    myDeliveryOption.innerHTML="Pick it from Pizza Shop";
+    myPizzaSize.innerHTML="Large";
 }
-if(deliveryYes)
+else if(size=="850")
 {
-    myDeliveryOption.innerHTML="Deliver it to me Ksh 150";   
+    myPizzaSize.innerHTML="Medium";    
+}
+else if(size=="600")
+{
+    myPizzaSize.innerHTML="Small";    
+}
+
+if(crust=="200")
+{
+    myCrust.innerHTML="Crispy @ sh. 200";
+}
+else if(crust=="150")
+{
+    myCrust.innerHTML="Stuffed @ sh. 150";    
+}
+else if(crust=="180")
+{
+    myCrust.innerHTML="Gluten-free @ sh. 180";    
+}
+
+if(delivery =="no")
+{
+    myDeliveryOption.innerHTML="";
+    amount.innerHTML="";
+    var valueSize1 = document.getElementById("crust").value; 
+    var valueCrust1 = document.getElementById("size").value; 
+
+    myDeliveryOption.innerHTML="Pick it from Pizza Shop";
+    var tot= +valueSize1 + +valueCrust1;
+    amount.innerHTML=tot;
+
+    
+
+}
+else if(delivery=="yes")
+{
+    myDeliveryOption.innerHTML="";
+    amount.innerHTML="";
+    var valueSize2 = document.getElementById("crust").value; 
+    var valueCrust2 = document.getElementById("size").value; 
+    myDeliveryOption.innerHTML="Deliver it to me Ksh 150";  
+    var tot=+valueSize2 + +valueCrust2+150;
+    amount.innerHTML=tot;
 }
 
 }
@@ -54,5 +97,6 @@ else
 }
 
 function placeOrder() {
-    
+ 
+    alert("Your order has been placed successfully!");   
 }
